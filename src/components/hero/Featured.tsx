c9771ac1f64a6ec5f78efe49bdf9164223191828
Kibiwott <bk.kibiwott@gmail.com>
2025-11-07 15:32:33 +0300

    Updated

--- src/components/hero/Featured.tsx
+++ src/components/hero/Featured.tsx
@@ -24,16 +24,17 @@ const Featured = () => {
   return (
     <>
         <h2 className="text-4xl font-bold text-primary pt-5 text-center mb-5">Featured Products</h2> 
-        <div className='w-full md: flex items-center justify-between gap-4 mt-3'>
+
+        <div className='w-full md: flex items-center justify-between gap-4 mt-3 p-5'>
             {
                 featuredProducts.map((product)=>(
-                    <div key={product.id} className='card w-full md:flex bg-blue-50  shadow-sm items-center pb-3  space-y-4'>
+                    <div key={product.id} className='card  bg-blue-50  shadow-sm items-center pb-3  space-y-4'>
                         
-                        <img src={product.image}  alt="Laptop" className='w-100 h-40 object-cover ' />
-                        
+                        <img src={product.image}  alt="Laptop" className='w-100 h-40 object-cover ' />                        
                         <h2 className='text-slate-900 text-2xl font-bold'>{product.name}</h2>
                         <h3 className='text-slate-700 text-1xl'>{product.price}</h3>
-                        <button className="btn btn-active btn-primary">Primary</button>
+
+                        <button className="btn btn-active  w-3/4 btn-primary ">Add to Cart</button>
                     </div>
                 ))
             }
