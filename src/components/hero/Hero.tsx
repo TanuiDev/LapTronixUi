6fed45b0ab19ff4ef799c5a2d227e89357bfc0ec
Kibiwott <bk.kibiwott@gmail.com>
2025-11-09 11:01:53 +0300

    Updated

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,11 +5,11 @@ import { NavLink } from "react-router";
 export const Hero = () => {
   return (
     <>
-      <div className="card rounded-lg bg-gray-800 mt-3 p-3 h-fit pl-4 bg-[url('https://images.unsplash.com/photo-1603302576837-37561b2e2302?w=400&h=300&fit=crop')]  bg-cover  bg-center  br-2 shadow-lg ">
+<div className="card rounded-lg bg-gray-800 mt-3 p-3 h-fit pl-4 bg-[url('https://i.pinimg.com/1200x/f3/80/d1/f380d1d910c3cfc744cbbaefd9fdd794.jpg')] bg-cover bg-center ">
         <h1 className="text-4xl font-bold text-secondary">
           Premium Tech at Your Fingertips
         </h1>
-        <p className="pt-4 text-gray-100  font-semibold h-60 sm:text-2xl md:text-4xl">
+        <p className="pt-4 text-inherit  font-semibold h-60 sm:text-2xl md:text-4xl">
           Discover the latest laptops, accessories, and tech gear from top
           brands. <br /> Quality you can trust, prices you'll love.
         </p>
@@ -24,3 +24,6 @@ export const Hero = () => {
     </>
   );
 };
+
+// https://i.pinimg.com/1200x/fd/83/c1/fd83c1a450cec817cfcd457cfa32c1cb.jpg
+// https://i.pinimg.com/736x/6d/30/d5/6d30d523884775494265c06e8bc4785d.jpg
